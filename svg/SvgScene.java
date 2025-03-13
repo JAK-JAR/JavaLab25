11a84d2b0df866069de35af7c5ed4cacde3c9ed2
Jakub Jarocki <jjarocki@ihp-szkola.pl>
2025-03-13 16:43:15 +0100

    changing features of my java program

--- svg/SvgScene.java
+++ svg/SvgScene.java
@@ -1,47 +1,45 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
-public class SvgScene 
-{
-        private Polygon[] polygons = new Polygon[3];
-        private int index = 0;
-        
-        public void addPolygon(Polygon polygon)
-        {   
-            if(index >= polygons.length)
-            {
-                polygons[(index++)%3] = polygon;
+
+public class SvgScene {
+    private Shape[] shapes = new Shape[3];
+    private int index = 0;
+
+    public void addShape(Shape shape) {
+        if (index >= shapes.length) {
+            shapes[(index++) % 3] = shape;
+        } else {
+            shapes[index++] = shape;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "SvgScene{" + "index=" + index + ", shapes=" + Arrays.toString(shapes) + '}';
+    }
+
+    public String toSvg() {
+        StringBuilder result = new StringBuilder("<svg xmlns=\"http://www.w3.org/2000/svg\">");
+        for (Shape shape : shapes) {
+            if (shape != null) {
+                result.append("\n\t").append(shape.toSvg());
             }
         }
+        result.append("\n</svg>");
+        return result.toString();
+    }
 
-        @Override
-        public String toString() 
-        {
-            return "SvgScene{" + "index" + index + ", polygons=" + Arrays.toString(polygons) + '}';
-        }
-
-        public String toSvg()
-        {
-        String result = "<svg xmlns=\"http://www.w3.org/2000/svg\">";
-        for(var polygon : polygons)
-        {
-            result += "\n\t" + polygon.toSvg();
-        }
-        result += "\n</svg>";
-        return result;
-        }
-
-        public void save(String filePath) throws IOException
-        {
+    public void save(String filePath) throws IOException {
         double xMin = Double.MAX_VALUE;
         double yMin = Double.MAX_VALUE;
         double xMax = Double.MIN_VALUE;
         double yMax = Double.MIN_VALUE;
 
-        // Oblicza rozmiar obrazu na podstawie boundingBox() wszystkich wielokątów
-        for (Polygon polygon : polygons) {
-            if (polygon != null) {
-                BoundingBox bbox = polygon.boundingBox();
+        // Oblicza rozmiar obrazu na podstawie boundingBox() wszystkich kształtów
+        for (Shape shape : shapes) {
+            if (shape != null) {
+                BoundingBox bbox = shape.boundingBox();
                 xMin = Math.min(xMin, bbox.x());
                 yMin = Math.min(yMin, bbox.y());
                 xMax = Math.max(xMax, bbox.x() + bbox.width());
@@ -54,18 +52,16 @@ public class SvgScene
 
         // Tworzy zawartość SVG z odpowiednimi rozmiarami
         StringBuilder svgContent = new StringBuilder(String.format("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"%f\" height=\"%f\">\n", width, height));
-        for (Polygon polygon : polygons) {
-            if (polygon != null) {
-                svgContent.append(polygon.toSvg()).append("\n");
+        for (Shape shape : shapes) {
+            if (shape != null) {
+                svgContent.append(shape.toSvg()).append("\n");
             }
         }
         svgContent.append("</svg>");
 
         // Zapisuje zawartość SVG do pliku
-        try (FileWriter writer = new FileWriter(filePath)) 
-        {
+        try (FileWriter writer = new FileWriter(filePath)) {
             writer.write(svgContent.toString());
         }
-        }
-
+    }
 }
